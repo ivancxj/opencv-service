cdff9ae8962897fe2be12a666d95f46627e32bd9
mugua <mugua@yaomaitong.cn>
2026-06-04 15:14:07 +0800

    ``` docs(start.sh): 添加构建说明注释
    添加关于修改main.py后需要重新构建镜像的说明注释，
    提醒开发者使用 docker compose up -d --build 命令进行构建。
    ```

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 # docker build -t opencv-service:4.13.0 -f Dockerfile .
 # docker rmi opencv-service:latest
 # docker tag opencv-service:4.13.0 opencv-service:latest
+
+# 如果修改了main.py 文件，请重新构建镜像
+# docker compose up -d --build 
 docker compose up -d
